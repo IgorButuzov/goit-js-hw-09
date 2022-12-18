dbd06f3e7a3ff34efe7bb4923fc761ae7b0558c0
Igor Butuzov <ibutuzov87@gmail.com>
2022-12-18 11:24:28 +0200

    task-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    bodyColor: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+}
 
+refs.startBtn.addEventListener('click', chgColor);
+refs.stopBtn.addEventListener('click', stopChgColor);
+
+function chgColor() {
+    intervalChgColor = setInterval(() => {
+    refs.bodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  refs.startBtn.setAttribute('disabled', 'true');
+}
+
+function stopChgColor() {
+    console.log('click');
+    clearInterval(intervalChgColor);
+    refs.startBtn.removeAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
